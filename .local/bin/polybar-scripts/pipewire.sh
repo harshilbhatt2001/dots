05c0fe8121b7fa4afda92a8e71bf0ef5570c267b
Harshil Bhatt <harshilbhatt2001@gmail.com>
2021-05-22 00:04:32 +0530

    update dot

--- .local/bin/polybar-scripts/pipewire.sh
+++ .local/bin/polybar-scripts/pipewire.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function main() {
-    DEFAULT_SOURCE=$(pw-record --list-targets | sed -n 's/^*[[:space:]]*[[:digit:]]\+: description="\(.*\)" prio=[[:digit:]]\+$/\1/p')
-    DEFAULT_SINK_ID=$(pw-play --list-targets | sed -n 's/^*[[:space:]]*\([[:digit:]]\+\):.*$/\1/p')
-    DEFAULT_SINK=$(pw-play --list-targets | sed -n 's/^*[[:space:]]*[[:digit:]]\+: description="\(.*\)" prio=[[:digit:]]\+$/\1/p')
-    VOLUME=$(pactl list sinks | sed -n "/Sink #${DEFAULT_SINK_ID}/,/Volume/ s!^[[:space:]]\+Volume:.* \([[:digit:]]\+\)%.*!\1!p" | head -n1)
-    IS_MUTED=$(pactl list sinks | sed -n "/Sink #${DEFAULT_SINK_ID}/,/Mute/ s/Mute: \(yes\)/\1/p")
+    # Pipewire
+    SOURCE=$(pw-record --list-targets | sed -n 's/^*.*"\(.*\)" prio=.*$/\1/p')
+    SINK=$(pw-play --list-targets | sed -n 's/^*.*"\(.*\)" prio=.*$/\1/p')
+    VOLUME=$(pactl list sinks | sed -n "/${SINK}/,/Volume/ s!^[[:space:]]\+Volume:.* \([[:digit:]]\+\)%.*!\1!p")
+    IS_MUTED=$(pactl list sinks | sed -n "/${SINK}/,/Mute/ s/Mute: \(yes\)/\1/p")
 
     action=$1
     if [ "${action}" == "up" ]; then
@@ -16,12 +16,11 @@ function main() {
         pactl set-sink-mute @DEFAULT_SINK@ toggle
     else
         if [ "${IS_MUTED}" != "" ]; then
-            echo " ${DEFAULT_SOURCE} |   MUTED ${DEFAULT_SINK}"
+            echo " ${SOURCE} |   MUTED ${SINK}"
         else
-            echo " ${DEFAULT_SOURCE} |    ${VOLUME}% ${DEFAULT_SINK}"
+            echo " ${SOURCE} |    ${VOLUME}% ${SINK}"
         fi
     fi
 }
 
-main "$@"
-
+main $@
