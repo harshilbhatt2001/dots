506fb8afffc23e4bee925ac4d2fdbf51dcac8644
Harshil Bhatt <harshilbhatt2001@gmail.com>
2021-05-21 11:34:19 +0530

    update dots

--- .local/bin/increase_brightness.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CurrentBrightness=$(cat "/sys/class/backlight/amdgpu_bl0/brightness")
-Increase=$1
-NewBrightness=$((CurrentBrightness+Increase))
-echo $NewBrightness | sudo tee /sys/class/backlight/amdgpu_bl0/brightness
