41af640d9bdf08c6ec2464984a19d36fc6a82fe6
Harshil Bhatt <harshilbhatt2001@gmail.com>
2023-09-09 15:26:25 +0530

    Update calendar.lua

--- .config/nvim/after/plugin/calendar.lua
+++ .config/nvim/after/plugin/calendar.lua
@@ -1,7 +1,7 @@
 vim.g.calendar_google_calendar = 1
 vim.g.calendar_google_task = 1
-vim.g.calendar_google_api_key = 'AIzaSyBz6UOE3hfjype1LL-j69ySgtYFTUo7Jng'
-vim.g.calendar_google_client_id = '62950133416-i4bndhm6jp2c68isvb3iai9osm66cqgc.apps.googleusercontent.com'
-vim.g.calendar_google_client_secret = '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'
+vim.g.calendar_google_api_key = '<>'
+vim.g.calendar_google_client_id = '<>'
+vim.g.calendar_google_client_secret = '<>'
 vim.g.calendar_date_endian = 'little'
 
